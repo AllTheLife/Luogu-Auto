93d8bfeec65e4a536d216b22bca0e2d3aad7e3c2
Wang Hongtian <redefinition0726@163.com>
2023-10-17 18:58:31 +0800

    更改邮件格式

--- luogu_punch_successfully_email.py
+++ luogu_punch_successfully_email.py
@@ -1,22 +1,30 @@
 import sys
+import datetime
 from smtplib import SMTP_SSL
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.header import Header
 
 
-def email(cookie, my_email, want_to_email):
+def email(key, my_email, to_email):
     host_server = 'smtp.163.com'
     sender_163 = my_email
-    pwd = cookie
-    receiver = [want_to_email]
+    pwd = key
+    receiver = [to_email]
     mail_title = '洛谷自动打卡成功'
-    mail_content = "洛谷自动打卡成功！\nby GitHub Action"
+    mail_head = """<div style = "text-align: center;">
+    <h2>洛谷自动打卡成功</h2>
+    <h4>By <a href = "https://github.com/CodingOIer/Luogu-Auto/">Luogu-Auto</a> on GitHub Action at """
+
+    mail_tail = """</h4>
+</div>
+"""
+    mail_content = mail_head + str(datetime.datetime.today()) + mail_tail
     msg = MIMEMultipart()
     msg["Subject"] = Header(mail_title, 'utf-8')
     msg["From"] = sender_163
     msg['To'] = ";".join(receiver)
-    msg.attach(MIMEText(mail_content, 'plain', 'utf-8'))
+    msg.attach(MIMEText(mail_content, 'html', 'utf-8'))
     smtp = SMTP_SSL(host_server)
     smtp.login(sender_163, pwd)
     smtp.sendmail(sender_163, receiver, msg.as_string())
@@ -24,4 +32,4 @@ def email(cookie, my_email, want_to_email):
 
 
 if __name__ == "__main__":
-    print(sys.argv[1],sys.argv[2])
+    email(sys.argv[1], sys.argv[2], sys.argv[3])
