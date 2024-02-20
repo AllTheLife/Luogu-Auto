889b9a76962a9fd8424ec7ec15121611a47728a5
CodingOIer <redefinition0726@163.com>
2024-02-20 15:00:31 +0800

    增加多用户支持

--- luogu_auto_punch.py
+++ luogu_auto_punch.py
@@ -1,9 +1,9 @@
 import requests
-import json
 import sys
 
-def punch(cookie):
-    return requests.get('https://www.luogu.com.cn/index/ajax_punch', headers={
+
+def makeHead(cookie):
+    temp = {
         "Host": "www.luogu.com.cn",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv: 73.0) Gecko/20100101 Firefox/73.0",
         "Accept": "*/*",
@@ -14,7 +14,18 @@ def punch(cookie):
         "Cache-Control": "no-cache",
         "TE": "Trailers",
         "Cookie": cookie
-    }).text
+    }
+    return temp
+
+
+def punch(cookie):
+    return requests.get('https://www.luogu.com.cn/index/ajax_punch', headers=makeHead(cookie)).text
+
 
 if __name__ == "__main__":
-    punch(sys.argv[1])
+    cookies = sys.argv[1]
+    pair = cookies.split('&')
+    for x in pair:
+        temp = x.split('|')
+        cookie = "__client_id=" + temp[0] + ";_uid=" + temp[1] + ";"
+        punch(cookie)
